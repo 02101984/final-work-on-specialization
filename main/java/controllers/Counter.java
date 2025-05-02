5c48feac8034adcf96edea4069f1f3c0ed4e16d1
nataliya <vasilenkonatalia02@gmail.com>
2025-05-02 21:47:27 +0300

    Добавить решение

---
+++ main/java/controllers/Counter.java
@@ -0,0 +1,19 @@
+package program.controllers;
+
+public class Counter implements AutoCloseable {
+
+    static int sum;
+
+    {
+        sum = 0;
+    }
+
+    public void add() {
+        sum++;
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Counter closed");
+    }
+}
---
+++ main/java/models/Pet.java
@@ -0,0 +1,45 @@
+package program.models;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public abstract class Pet {
+
+    protected int petId;
+    protected String nickname;
+    protected LocalDate birthday;
+
+    public int getPetId() {
+        return petId;
+    }
+
+    public void setPetId(int petId) {
+        this.petId = petId;
+    }
+
+    public String getName() {
+        return nickname;
+    }
+
+    public void setName(String name) {
+        this.nickname = name;
+    }
+
+    public LocalDate getBirthdayDate() {
+        return birthday;
+    }
+
+    public String getBirthday() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return formatter.format(birthday);
+    }
+
+    public void setBirthday(LocalDate date) {
+        this.birthday = date;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d. %s: кличка: %s, дата рождения: %s ", getPetId(), getClass().getSimpleName(), nickname, getBirthday());
+    }
+}
